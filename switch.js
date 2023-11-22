c9251144ed718cdda4f3cd8c18b25c18dfde38c2
Bearcin46 <imsweety33@gmail.com>
2023-11-22 16:09:03 +0530

    Switch statement

--- switch.js
+++ switch.js
@@ -38,3 +38,29 @@ console.log(localtime)
 
 //if tou use number then switch(true)
 //case mark >6
+
+let mark=98;
+
+switch(true){
+    case mark >90:
+        console.log("Hey Congrats! You are a Topper, You got 'S' Grade");
+        break;
+    case mark <=90 && mark >80:
+        console.log("Congrats you got 'A' Grade");
+        break;
+    case mark <=80 && mark >70:
+        console.log("You got 'B' Grade");
+        break;
+    case mark <=70 && mark>60:
+        console.log("You got 'C' Grade");
+        break;
+    case mark <= 60 && mark >50:
+        console.log("You got 'D' Grade");
+        break;
+    case mark <= 50 && mark >45:
+        console.log("You got 'E' Grade");
+        break;
+    default:
+        console.log("Hard Luck! Try next Time, You got 'U' Grade");
+    
+}
